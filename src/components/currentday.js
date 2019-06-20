28153b04b15447516f8f30eb13d7636a04427d16
unknown <piotr.w.sroda@gmail.com>
2019-06-20 22:04:56 +0200

    small change in Css

--- src/components/currentday.js
+++ src/components/currentday.js
@@ -6,11 +6,13 @@ const CurrentDay = props => {
     .hour(`${props.occupied}`)
     .minutes(0)
     .format("LT");
+
   let date = moment(props.day).format("ll");
   let time = [];
   let startHour = props.startHour * 60;
   let endHour = props.endHour * 60;
   let step = props.step;
+
   for (let i = startHour; i <= endHour; i += step) {
     let minutes = moment(props.day)
       .add(i, "minutes")
@@ -20,7 +22,12 @@ const CurrentDay = props => {
   console.log(occupiedHour);
 
   const renderTime = time.map((t, k) => {
-    if (t === occupiedHour) return <div>OCCUPIED</div>;
+    if (t === occupiedHour)
+      return (
+        <div key={k} className="timesheet_item_unclickable">
+          {t}
+        </div>
+      );
     return (
       <div key={k} className="timesheet_item">
         {t}
